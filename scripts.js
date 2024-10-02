3c20e598a658fb52c93252bc407b3f8d946bd6f6
PedroHL19 <pedrohlopes1903@gmail.com>
2024-10-02 17:31:26 -0300

    hehe

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function mudarCor() {
     const corSelecionada = document.getElementById("seletorCor").value;
-    document.body.style.color = corSelecionada; // Muda a cor do texto de toda a página
+    document.body.style.color = corSelecionada;
 }
